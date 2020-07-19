b5b32ec1a1aec3e249c0455620c08e8cc32ccbb2
marinarediko <ma.rediko@gmail.com>
2020-07-19 15:52:19 +0300

    Snake_Ver_20.07.19_01

---
+++ SnakeGame/Food.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    class Food : PictureBox
+    {
+        Random rand = new Random();
+        public Food()
+        {
+            InitializeFood();           
+        }
+
+        private void InitializeFood()
+        {
+            this.Width = 20;
+            this.Height = 20;
+            this.BackColor = Color.DarkOrange;
+        }
+        public void GetFoodLocation()
+        {
+            this.Location = new Point(100 + 20 * rand.Next(0, 20), 100 + 20 * rand.Next(0, 20));
+        }
+
+    }
+}
